d0b49be05c1d577d3b82aae568d2928df08eec1b
Yi Chen <easoncy435@gmail.com>
2015-12-08 00:35:39 -0500

    birthday

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('compatibilityAstrologer', ['ngRoute', 'appRoutes', 'ApiService']);
+var app = angular.module('compatibilityAstrologer', ['ngRoute', 'appRoutes', 'ApiService','ui.bootstrap']);
 var config = {
 	base_url: 'http://127.0.0.1',
 	port: 5000
